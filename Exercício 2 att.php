fbb8b19d62f9c8672712bf83f482a0cea574f9e5
Niayou <113477124+Niayou@users.noreply.github.com>
2022-09-13 19:05:15 -0300

    Add files via upload

---
+++ Exercício 2 att.php
@@ -0,0 +1,8 @@
+<?php
+$numeros = [53, 82, 80, 66, 41, 64, 28, 99, 77, 57];
+
+    echo "Maior: " . max($numeros) . "<br>";
+    echo "Menor: " . min($numeros) . "<br>";
+    echo "Média: " . (array_sum($numeros)) / count($numeros) . "<br>";
+
+?>
